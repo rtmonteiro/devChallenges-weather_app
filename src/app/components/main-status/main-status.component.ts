27e3a0420f0e0abe07b6477f07124d2e2c23abe0
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2021-07-31 21:41:20 -0300

    feat: styling main status

--- src/app/components/main-status/main-status.component.ts
+++ src/app/components/main-status/main-status.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'wt-main-status',
   templateUrl: './main-status.component.html',
   styleUrls: ['./main-status.component.scss']
